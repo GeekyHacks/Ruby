99f01b5f109314ab8dad51c48718e57c4ba37dac
GeekyHacks <geekyhacks22@gmail.com>
2023-03-13 17:55:05 +0300

    push

---
+++ notes&practices/classAndMethodsSyntax.rb
@@ -0,0 +1,16 @@
+class User
+    name = "abdu"
+    age = 29
+    work = "programmer"
+
+    #methods
+    def first_metod
+        "its not easy to be a prgrammer especially if you start it older"
+        
+    end
+
+end
+
+user = User.new
+
+p user.first_metod
